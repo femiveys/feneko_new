51478d576184847e323c421c9a22e0738706c424
Esther De Jonghe <estherdejonghe@MacBook-Pro-van-Esther.local>
2015-08-12 23:19:21 +0200

    ckeditor style check

--- sites/all/themes/feneko/js/ckeditor.styles.js
+++ sites/all/themes/feneko/js/ckeditor.styles.js
@@ -1,13 +1,19 @@
-CKEDITOR.addStylesSet( 'drupal', [ 
-	
-	/* Block Styles */ 
-	{ name : 'Titel 2' , element : 'h2' }, 
-	{ name : 'Titel 3' , element : 'h3' }, 
-	{ name : 'paragraaf' , element : 'p' }, 
-	{ name : 'knop' , 
+CKEDITOR.addStylesSet( 'drupal', [
+
+	/* Block Styles */
+	{ name : 'Titel 2' , element : 'h2' },
+	{ name : 'Titel 3' , element : 'h3' },
+	{ name : 'paragraaf' , element : 'p' },
+	{ name : 'knop' ,
 		element : 'div',
 		attributes: {
-			'class' : 'ck-knop' 
-		} 
+			'class' : 'ck-knop'
+		}
+	},
+	{ name : 'check' ,
+		element : 'div',
+		attributes: {
+			'class' : 'ck-check'
+		}
 	}
-]); 
+]);
