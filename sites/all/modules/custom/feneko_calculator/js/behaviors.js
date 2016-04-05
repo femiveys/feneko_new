f5c16788668e005e411bff4f061791a3cf461e93
Femi Veys <femi@itsimplyworks.be>
2016-04-05 11:41:18 +0200

    Add dorpel eindstuk images

--- sites/all/modules/custom/feneko_calculator/js/behaviors.js
+++ sites/all/modules/custom/feneko_calculator/js/behaviors.js
@@ -60,6 +60,16 @@
       $('.commerce-add-to-cart').each(function(value, key) {
         // $(this).find('.form-item-quantity label').replaceWith($(this).find('.form-submit'));
       });
+
+      // Add the dorpel images: This is dirty
+      var selector = '.node-raamtablet .commerce-add-to-cart #field-eindstukken-values ';
+      var p = '<td rowspan="2" class="thumb">';
+      var eindstukImage1 = $('.commerce-add-to-cart img[title$=1]');
+      var eindstukImage2 = $('.commerce-add-to-cart img[title$=2]');
+      $(selector + 'tbody tr:nth-child(1)').prepend(p).find('td').first().prepend(eindstukImage1);
+      $(selector + 'tbody tr:nth-child(3)').prepend(p).find('td').first().prepend(eindstukImage2);
+
+      $(selector + 'thead th').attr('colspan', 2); // As we have added a cell, we need to add a colspan
     }
   };
 
