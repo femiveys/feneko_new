c97e7d31fa42fac3fedc41159cd4a2a42077f76c
Femi Veys <femi@itsimplyworks.be>
2016-06-29 15:05:50 +0200

    remove sendgrid

--- sites/all/vendor/composer/autoload_namespaces.php
+++ sites/all/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname(dirname($vendorDir)).'/default/files/composer';
 
 return array(
-    'Smtpapi' => array($vendorDir . '/sendgrid/smtpapi/lib'),
-    'SendGrid' => array($vendorDir . '/fastglass/sendgrid/lib'),
 );
